1a7f68fc7daae1bf6c07ccef29ea145b682a1c06
Ali Asghar M. Akmad <40554152+aliitotz@users.noreply.github.com>
2021-03-05 10:56:32 +0800

    Update ContactManager.java
    -Revert changes

--- src/android/ContactManager.java
+++ src/android/ContactManager.java
@@ -252,18 +252,6 @@ public class ContactManager extends CordovaPlugin {
 
                 try {
                     JSONObject contact = contactAccessor.getContactById(id);
-                       
-                     AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                     alertDialog.setTitle("Alert");
-                     alertDialog.setMessage("Alert message to be shown");
-                     alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                         new DialogInterface.OnClickListener() {
-                             public void onClick(DialogInterface dialog, int which) {
-                                 dialog.dismiss();
-                             }
-                         });
-                     alertDialog.show();
-                       
                     this.callbackContext.success(contact);
                     return;
                 } catch (JSONException e) {
