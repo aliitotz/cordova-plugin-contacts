65c29c72db79871ea8b7bdbe4039c0b8d96d9c06
Ali Asghar M. Akmad <40554152+aliitotz@users.noreply.github.com>
2021-03-05 10:48:03 +0800

    Update ContactManager.java
    -Removed JOption message
    -Added AlertMessage

--- src/android/ContactManager.java
+++ src/android/ContactManager.java
@@ -37,7 +37,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.RawContacts;
-import static javax.swing.JOptionPane.showMessageDialog;
 
 import java.lang.reflect.Method;
 
@@ -253,7 +252,18 @@ public class ContactManager extends CordovaPlugin {
 
                 try {
                     JSONObject contact = contactAccessor.getContactById(id);
-                    JOptionPane.showMessageDialog(null, "Contacts: " + contact);
+                       
+                     AlertDialog alertDialog = new AlertDialog.Builder(this.cordova.getActivity()).create();
+                     alertDialog.setTitle("Alert");
+                     alertDialog.setMessage("Alert message to be shown");
+                     alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                         new DialogInterface.OnClickListener() {
+                             public void onClick(DialogInterface dialog, int which) {
+                                 dialog.dismiss();
+                             }
+                         });
+                     alertDialog.show();
+                       
                     this.callbackContext.success(contact);
                     return;
                 } catch (JSONException e) {
